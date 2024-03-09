6f65fd51530e8cf3932e2d840afc15d16d967747
Ishan Shishodiya <sly.of.zero@gmail.com>
2024-03-09 18:12:21 +0530

    fixes

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,5 @@
 export async function apiFetcher<T>(url: string) {
   const response = await fetch(url);
-  console.log(await response.text());
   const data = (await response.json()) as T;
 
   return { response: response.status, data };
